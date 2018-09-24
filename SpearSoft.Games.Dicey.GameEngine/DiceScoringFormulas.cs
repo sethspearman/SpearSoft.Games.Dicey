935848013f9b55b08dbab66a400dd61210c0de11
Seth Spearman <sethspearman@gmail.com>
2018-09-24 16:33:38 -0400

    added scoring formulas

--- SpearSoft.Games.Dicey.GameEngine/DiceScoringFormulas.cs
+++ SpearSoft.Games.Dicey.GameEngine/DiceScoringFormulas.cs
@@ -9,5 +9,28 @@ namespace SpearSoft.Games.Dicey.GameEngine
         {
             return dice => dice.Where(d => d == valueToCheck).Sum(d => d);
         }
+
+        public static Func<byte[], int> DiceSumAll()
+        {
+            return dice => dice.Sum(d => d);
+        }
+
+        public static Func<byte[], int> DiceReturnSpecificValue(int valueToReturn)
+        {
+            return dice => valueToReturn;
+        }
+
+        public static Func<byte[], int> DiceSumAllIfValid(Func<byte[], bool> validator)
+        {
+            //return dice => dice.Where(d => d == valueToCheck).Sum(d => d);
+            return GetValue;
+        }
+
+        private static int GetValue(byte[] bytes)
+        {
+            //Func<byte[], bool> Validator = 
+
+            return 10;
+        }
     }
 }
