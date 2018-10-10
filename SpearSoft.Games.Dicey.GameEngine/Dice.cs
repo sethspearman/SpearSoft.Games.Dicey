9cc6f69acc0ff691aa306ad622c2613244d8eecd
Seth <sethspearman@gmail.com>
2018-10-10 00:46:50 -0400

    equal must return false on comparison to null.

--- SpearSoft.Games.Dicey.GameEngine/Dice.cs
+++ SpearSoft.Games.Dicey.GameEngine/Dice.cs
@@ -16,23 +16,6 @@ namespace SpearSoft.Games.Dicey.GameEngine
 
         public static Dice Empty => new Dice(true);
 
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                var bytes = ToByteArray();
-
-                var hash = 13;
-                hash = hash * 7 + bytes[0].GetHashCode();
-                hash = hash * 7 + bytes[1].GetHashCode();
-                hash = hash * 7 + bytes[2].GetHashCode();
-                hash = hash * 7 + bytes[3].GetHashCode();
-                hash = hash * 7 + bytes[4].GetHashCode();
-
-                return hash;
-            }
-        }
-
         public void Roll()
         {
             foreach (var die in this) die.Roll();
@@ -62,12 +45,37 @@ namespace SpearSoft.Games.Dicey.GameEngine
         }
 
         #region System Overrides (GetHashCode, Equals, and == and != operators)
-        // all this because I want to be able to do Dice dice = Dice.Empty;
 
+        // all this because I want to be able to do Dice dice = Dice.Empty;
+        
+        // https://stackoverflow.com/questions/371328/why-is-it-important-to-override-gethashcode-when-equals-method-is-overridden
         public override bool Equals(object obj)
         {
+            // Equals must return false on comparison to null
+            // http://msdn.microsoft.com/en-us/library/bsc2ak47.aspx
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
             var dice = obj as Dice;
             return dice != null && dice.GetHashCode() == GetHashCode();
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var bytes = ToByteArray();
+
+                var hash = 13;
+                hash = hash * 7 + bytes[0].GetHashCode();
+                hash = hash * 7 + bytes[1].GetHashCode();
+                hash = hash * 7 + bytes[2].GetHashCode();
+                hash = hash * 7 + bytes[3].GetHashCode();
+                hash = hash * 7 + bytes[4].GetHashCode();
+
+                return hash;
+            }
         }
 
         public static bool operator ==(Dice d1, Dice d2)
